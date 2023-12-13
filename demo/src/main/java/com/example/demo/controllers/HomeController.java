19c64fbb003c07a4725c28d72c8dc6665861fdf0
goshy <georgi.dobromirov@gmail.com>
2023-12-13 08:22:20 +0200

    some changes

--- demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String getHome() {
+        //https://www.youtube.com/watch?v=YZZbl5yA_Hs&list=PLp4DtrLRhb24V67po4dhTMiVm2YBYGmlF&index=29
         return "Hello from Home page in 25 Hibernate and SpringData! :))";
     }
 }
